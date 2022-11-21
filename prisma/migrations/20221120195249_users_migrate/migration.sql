cbb61fbfa7c154817526d97d3c5e5273e9d11765
ganefiatri <gicerf@gmail.com>
2022-11-21 23:23:00 +0700

    Full Auth System

---
+++ prisma/migrations/20221120195249_users_migrate/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `firstName` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `isActive` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `lastName` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `registeredAt` on the `users` table. All the data in the column will be lost.
+  - Added the required column `username` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `users` DROP COLUMN `firstName`,
+    DROP COLUMN `isActive`,
+    DROP COLUMN `lastName`,
+    DROP COLUMN `registeredAt`,
+    ADD COLUMN `username` VARCHAR(250) NOT NULL;
